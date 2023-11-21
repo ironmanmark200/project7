71685bb828d0d06dc8c9227bf7940062653b4c8c
ironmanmark200 <143750816+ironmanmark200@users.noreply.github.com>
2023-11-20 18:28:08 -0700

    Update myscripts.js

---
+++ project 7/myscripts.js
@@ -0,0 +1,39 @@
+var imgArray = new Array(
+			'CoupleSailingLg.jpg',
+			'PigLg.jpg',
+			'SaladLg.jpg',
+			'StorkLg.jpg'
+		);
+
+
+		var titleArray = new Array(
+			'Newlyweds Sailing',
+			'Boy and His Pig',
+			'Yummy Salad',
+			'Stork at Sunset'
+		);
+			
+		var imgPath = "Images/Fullsize/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+      			textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
